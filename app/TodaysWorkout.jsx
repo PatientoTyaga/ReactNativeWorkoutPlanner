b3f7315aa82d1cd62ac6fef53f7171986ba4f3c3
PatientoTyaga <47284708+PatientoTyaga@users.noreply.github.com>
2024-09-03 20:22:42 -0500

    native wind setup
    installing native wind for styling

---
+++ app/TodaysWorkout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TodaysWorkout = () => {
+  return (
+    <View>
+      <Text>TodaysWorkout</Text>
+    </View>
+  )
+}
+
+export default TodaysWorkout
+
+const styles = StyleSheet.create({})
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>WorkoutPlanner</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-4xl">WorkoutPlanner</Text>
       <StatusBar style="auto" />
+      <Link href="/TodaysWorkout" style={{ color: 'blue' }}>Today's Workout</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
